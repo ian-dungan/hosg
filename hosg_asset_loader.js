7827833f9f3796e830c494e7a56cdaf7930121a6
ian-dungan <iandungan@me.com>
2025-11-23 19:37:04 -0600

    Update wolf enemy scale in asset loader

--- hosg_asset_loader.js
+++ hosg_asset_loader.js
@@ -36,7 +36,7 @@ class AssetLoader {
       
       'enemy_wolf': {
         url: 'https://raw.githubusercontent.com/ian-dungan/hosg/main/assets/enemies/common/Wolf.glb',
-        scale: 0.35,
+        scale: 0.02,
         castShadows: true,
         collisions: true
       },
