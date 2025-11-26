7f9de884432e1c8b75e75b47d75c2eacfbcf8c5f
ian-dungan <iandungan@me.com>
2025-11-26 10:32:05 -0600

    Update world.js

--- js/world.js
+++ js/world.js
@@ -1,6 +1,4 @@
 // world.js - World management
-// Using BABYLON namespace directly
-
 class World {
     constructor(scene) {
         this.scene = scene;
@@ -15,16 +13,44 @@ class World {
     }
 
     createGround() {
-        // Create a simple ground
+        // Create a simple green ground
         this.ground = BABYLON.MeshBuilder.CreateGround('ground', {
             width: 100,
-            height: 100
+            height: 100,
+            subdivisions: 20  // More subdivisions for better lighting
         }, this.scene);
         
         const groundMaterial = new BABYLON.StandardMaterial('groundMaterial', this.scene);
-        groundMaterial.diffuseColor = new BABYLON.Color3(0.2, 0.6, 0.3);
+        groundMaterial.diffuseColor = new BABYLON.Color3(0.2, 0.6, 0.3); // Green color
+        groundMaterial.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1); // Reduce shininess
+        
+        // Add some basic bumpiness
+        groundMaterial.bumpTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/grassn.png", this.scene);
+        groundMaterial.bumpTexture.level = 0.2;
+        
         this.ground.material = groundMaterial;
+        this.ground.receiveShadows = true;
         this.ground.checkCollisions = true;
+    }
+
+    createSkybox() {
+        const skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 1000.0 }, this.scene);
+        const skyboxMaterial = new BABYLON.StandardMaterial("skyBox", this.scene);
+        skyboxMaterial.backFaceCulling = false;
+        
+        // Create a simple gradient sky
+        skyboxMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.7, 1.0); // Light blue
+        skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+        skyboxMaterial.disableLighting = true; // Sky should not be affected by lights
+        
+        // Add some simple clouds
+        const cloudTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/cloud.png", this.scene);
+        cloudTexture.level = 0.2; // Slight cloud effect
+        skyboxMaterial.reflectionTexture = cloudTexture;
+        skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+        
+        skybox.material = skyboxMaterial;
+        return skybox;
     }
 
     loadInitialChunks() {
