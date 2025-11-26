a0508e5b77edb1d17154029bb24c3edb8e23484e
ian-dungan <iandungan@me.com>
2025-11-26 10:01:45 -0600

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,62 +1,71 @@
-// player.js - Player controller
-import { Vector3, MeshBuilder } from '@babylonjs/core';
+// player.js
+// Using global BABYLON object from CDN
+const { Vector3, MeshBuilder } = BABYLON;
 
-export class Player {
+class Player {
     constructor(scene) {
         this.scene = scene;
-        this.position = Vector3.Zero();
-        this.velocity = Vector3.Zero();
-        this.rotation = 0;
+        this.mesh = null;
         this.health = CONFIG.PLAYER.START_HEALTH;
         this.mana = CONFIG.PLAYER.START_MANA;
-        
+        this.position = new Vector3(0, 2, 0); // Start slightly above ground
+        this.velocity = new Vector3(0, 0, 0);
+        this.isJumping = false;
+        this.isRunning = false;
         this.init();
     }
 
     init() {
         // Create player mesh
-        this.mesh = MeshBuilder.CreateCylinder('player', {
-            height: 2,
-            diameter: 1
+        this.mesh = MeshBuilder.CreateBox('player', { 
+            width: 1, 
+            height: 2, 
+            depth: 1 
         }, this.scene);
-        
-        // Set initial position
-        this.mesh.position = this.position.clone();
+        this.mesh.position = this.position;
+        this.mesh.checkCollisions = true;
     }
 
     update(deltaTime) {
-        // Update position based on velocity
-        this.position.addInPlace(this.velocity.scale(deltaTime));
-        this.mesh.position.copyFrom(this.position);
-        
         // Apply gravity
         this.velocity.y += CONFIG.PLAYER.GRAVITY * deltaTime;
         
+        // Update position
+        this.position.addInPlace(this.velocity.scale(deltaTime));
+        this.mesh.position.copyFrom(this.position);
+        
         // Simple ground collision
-        if (this.position.y < 0) {
-            this.position.y = 0;
+        if (this.position.y < 1) {
+            this.position.y = 1;
             this.velocity.y = 0;
+            this.isJumping = false;
         }
     }
 
     move(direction) {
-        const speed = CONFIG.PLAYER.MOVEMENT_SPEED;
-        this.velocity.x = direction.x * speed;
-        this.velocity.z = direction.z * speed;
+        // Move player in the given direction
+        const moveSpeed = CONFIG.PLAYER.MOVEMENT_SPEED * 
+                         (this.isRunning ? CONFIG.PLAYER.RUN_MULTIPLIER : 1);
+        this.velocity.x = direction.x * moveSpeed;
+        this.velocity.z = direction.z * moveSpeed;
     }
 
     jump() {
-        if (Math.abs(this.position.y) < 0.1) {
+        if (!this.isJumping) {
             this.velocity.y = CONFIG.PLAYER.JUMP_FORCE;
+            this.isJumping = true;
         }
     }
 
     takeDamage(amount) {
         this.health = Math.max(0, this.health - amount);
-        return this.health <= 0;
+        return this.health > 0;
     }
 
     dispose() {
-        this.mesh.dispose();
+        if (this.mesh) {
+            this.mesh.dispose();
+            this.mesh = null;
+        }
     }
 }
