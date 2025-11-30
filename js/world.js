99f17ebdef58161a55b47bccab0c583e67076a7f
ian-dungan <iandungan@me.com>
2025-11-30 14:49:25 -0600

    Update world.js

--- js/world.js
+++ js/world.js
@@ -115,7 +115,7 @@ class World {
 
     createSkybox() {
         // Try to load custom HDRI skybox
-        const skyPath = 'assets/sky/DaySkyHDRI015B_4K_TONEMAPPED.jpg';
+        const skyPath = 'assets/sky/DaySkyHDRI023B_4K_TONEMAPPED.jpg';
         
         try {
             // Use PhotoDome for 360° panoramic skybox
