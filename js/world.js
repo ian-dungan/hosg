61802e7bf1d9e56c6cbd682b1541fb444ecee0d0
ian-dungan <iandungan@me.com>
2025-11-30 13:59:30 -0600

    Update world.js

--- js/world.js
+++ js/world.js
@@ -1109,7 +1109,7 @@ class World {
         
         // Animate water bump texture if it exists
         if (this.waterMaterial.bumpTexture) {
-            const time = Date.now() * 0.0005;
+            const time = Date.now() * 0.001;
             this.waterMaterial.bumpTexture.uOffset += 0.001;
             this.waterMaterial.bumpTexture.vOffset += 0.001;
         }
