c8b3e8caed1cbd798f271ece6b506c326bcdb681
ian-dungan <iandungan@me.com>
2025-12-10 19:10:03 -0600

    Update assets.js

--- js/assets.js
+++ js/assets.js
@@ -43,6 +43,7 @@ const ASSET_PATHS = {
     // Generic models (shared objects)
     GENERIC_MODELS: {
         // Example: tree: "tree_01.glb"
+        tree: "tree_01.gltf"
     },
     
     // Environment textures (all in environment folder)
